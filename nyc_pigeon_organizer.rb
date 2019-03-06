af97dc3629df658fbaaf4a383ee5713f37a5812d
Samuel Guo <guosamuel1114@gmail.com>
2019-03-06 17:13:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,49 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	pigeon_names = []
+	pigeon_list = {}
+
+	data.each do |i, j|
+		j.each do |a, b|
+			pigeon_names.push(b)
+		end
+	end
+
+	unique_pigeon_names = pigeon_names.flatten.uniq
+	
+	unique_pigeon_names.each do |name|
+		data.each do |c, d|
+			d.each do |e, f|
+				if f.include?(name)
+				  if e.is_a? Symbol 
+				    if !pigeon_list[name]
+				      # binding.pry
+					    pigeon_list[name] = {c => [e.id2name]}
+					  else
+					    if pigeon_list[name][c]
+					      pigeon_list[name][c].push(e.id2name)
+					    else
+					      pigeon_list[name].merge!({c => [e.id2name]})
+					    end
+					  end
+					else
+					 # pigeon_list[name][c].push(e)
+					  pigeon_list[name].merge!({c => [e]})
+					end
+				end
+			end
+		end
+	end
+	
+  
+# 	pigeon_list.each do |pigeon_name, attribute|
+# 		data[:color].each do |color, bleh|
+# 				if bleh.include?(pigeon_name) && attribute[:color][0] != color.id2name
+# 					attribute[:color].push(color.id2name)
+# 				end
+# 		end
+# 	end
+
+	pigeon_list
 end
